cee05847d25e75d38f1e265acb56744b62221f6f
mitchellgilesdev <47744016+mitchellgilesdev@users.noreply.github.com>
2019-08-30 13:45:48 +0800

    init

---
+++ happiness_index.py
@@ -0,0 +1,9 @@
+def main():
+    file_name = str(input("Enter the name of the file containing World Happiness computation data: "))
+    metric = str(input("Choose metric to be tested from: min, mean, median, harmonic_mean. "))
+    output_action = str(input(
+        "Choose action to be performed on the data using the specified metric. The options are: list, correlation. "))
+
+
+
+main()
